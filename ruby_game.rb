4c5ffe1e1d7a45ffcfa7ecdaa5713bea3644e0c6
Solo Rakotozaka <s.rakotozaka@lagardere-tr.com>
2017-07-03 16:05:37 +0200

    Utilisation de each

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,8 +1,8 @@
 def draw(nb_rows, nb_cols, player)
 nb_cols = 5
 nb_rows = 3
-  for i in 1..nb_rows
-    for j in 1..nb_cols
+  (1..nb_rows).each do |i|
+    (1..nb_cols).each do |j|
       if player[:x]==i and player[:y]==j
         print "P"
       else
