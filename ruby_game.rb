3c57aa8de36c69ecb937399e19007a0956c526d9
Solo Rakotozaka <s.rakotozaka@lagardere-tr.com>
2017-07-03 12:19:34 +0200

    Positionnement player

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,8 +1,14 @@
 nb_cols = 5
 nb_rows = 3
+player_x = 3
+player_y = 2
 for i in 1..nb_rows
   for j in 1..nb_cols
-    print "."
+    if player_x==i && player_y==j
+      print "P"
+    else
+      print "."
+    end
   end
     print "\n"
 end
