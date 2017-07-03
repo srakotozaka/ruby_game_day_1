0717df9c3f45075b8702f72f447818a3f418d334
Solo Rakotozaka <s.rakotozaka@lagardere-tr.com>
2017-07-03 14:51:16 +0200

    utilisation symbol

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,9 +1,9 @@
 nb_cols = 5
 nb_rows = 3
-player = {"x"=>3,"y"=>2}
+player = {x: 3,y: 2}
 for i in 1..nb_rows
   for j in 1..nb_cols
-    if player["x"]==i and player["y"]==j
+    if player[:x]==i and player[:y]==j
       print "P"
     else
       print "."
