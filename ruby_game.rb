4f9227ebc3f30d0e6dccce79f42bdfa4006be2b8
Solo Rakotozaka <s.rakotozaka@lagardere-tr.com>
2017-07-03 12:04:27 +0200

    Utilisation for

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,14 +1,8 @@
-#jkhgkjhg
-index = 0
-indexrows = 0
 nb_cols = 5
-nb_rows=3
-while indexrows < nb_rows
-  index = 0
-  while index < nb_cols
+nb_rows = 3
+for i in 1..nb_rows
+  for j in 1..nb_cols
     print "."
-    index +=1
   end
-  indexrows+=1
-  print "\n"
+    print "\n"
 end
